c205377f8592eeadd93052a7ac8cc7c06a96cba7
leonardossev <leonardossev@gmail.com>
2023-10-01 18:31:20 -0300

    Adding the persistence exception

---
+++ src/main/java/com/leonardossev/ride/shared/exceptions/PersistenceException.java
@@ -0,0 +1,8 @@
+package com.leonardossev.ride.shared.exceptions;
+
+public class PersistenceException extends RuntimeException {
+
+    public PersistenceException(String msg) {
+        super(msg);
+    }
+}
