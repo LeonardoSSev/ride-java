5faa0148c681bc4f296ab800181df25a12341fdf
leonardossev <leonardossev@gmail.com>
2023-10-01 19:17:07 -0300

    Creating service for get by id port

---
+++ src/main/java/com/leonardossev/ride/core/services/GetAccountByIdService.java
@@ -0,0 +1,24 @@
+package com.leonardossev.ride.core.services;
+
+import com.leonardossev.ride.core.model.Account;
+import com.leonardossev.ride.core.ports.inbound.GetAccountByIdInboundPort;
+import com.leonardossev.ride.core.ports.outbound.AccountPersistenceOutboundPort;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class GetAccountByIdService implements GetAccountByIdInboundPort {
+
+    private final AccountPersistenceOutboundPort accountPersistenceOutboundPort;
+
+    public GetAccountByIdService(AccountPersistenceOutboundPort accountPersistenceOutboundPort) {
+        this.accountPersistenceOutboundPort = accountPersistenceOutboundPort;
+
+    }
+
+    @Override
+    public Optional<Account> execute(String uuid) {
+        return this.accountPersistenceOutboundPort.findById(UUID.fromString(uuid));
+    }
+}
