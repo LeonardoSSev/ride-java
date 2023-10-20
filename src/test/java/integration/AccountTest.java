9ade3b3055b75c347ef53975e5e151539dc4504f
leonardossev <leonardossev@gmail.com>
2023-10-20 17:32:05 -0300

    Adding integration tests for account

--- src/test/java/integration/AccountTest.java
+++ src/test/java/integration/AccountTest.java
@@ -7,12 +7,11 @@ import com.leonardossev.ride.core.ports.outbound.AccountPersistenceOutboundPort;
 import com.leonardossev.ride.core.ports.outbound.SendEmailOutboundPort;
 import com.leonardossev.ride.core.services.GetAccountByIdService;
 import com.leonardossev.ride.core.services.SignupService;
+import com.leonardossev.ride.core.services.helper.SignupAccountTestsHelper;
 import com.leonardossev.ride.core.validators.CpfValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,108 +20,88 @@ import static org.mockito.Mockito.mock;
 
 public class AccountTest {
 
-//    private SignupService signupService;
-//    private GetAccountByIdService getAccountByIdService;
-//
-//    @BeforeEach
-//    public void setUp() {
-//        AccountPersistenceOutboundPort accountPersistenceOutboundPort = new InMemoryAccountClientAdapter();
-//        SendEmailOutboundPort sendEmailOutboundPort = mock(SendEmailOutboundPort.class);
-//
-//        signupService = new SignupService(new CpfValidator(), accountPersistenceOutboundPort, sendEmailOutboundPort);
-//        getAccountByIdService = new GetAccountByIdService(accountPersistenceOutboundPort);
-//    }
-//
-//    @Test
-//    public void shouldCreatePassenger() {
-//        SignupAccount input = buildPassengerAccount();
-//
-//        String accountId = signupService.execute(input);
-//        Optional<Account> accountOptional = getAccountByIdService.execute(accountId);
-//
-//        assertTrue(accountOptional.isPresent());
-//        Account account = accountOptional.get();
-//        assertEquals(input.get("name"), account.name());
-//        assertEquals(input.get("email"), account.email());
-//        assertEquals(input.get("cpf"), account.cpf());
-//    }
-//
-//    private SignupAccount buildPassengerAccount() {
-//        return SignupAccount.builder()
-//                .name("John Doe")
-//                .email("john.doe" + Math.random() + "@gmail.com")
-//                .cpf("95818705552")
-//                .isPassenger(true)
-//                .build();
-//    }
-//
-//    @Test
-//    public void shouldNotCreatePassengerWithInvalidCpf() {
-//        Map<String, Object> input = new HashMap<>();
-//        input.put("name", "John Doe");
-//        input.put("email", "john.doe" + Math.random() + "@gmail.com");
-//        input.put("cpf", "95818705500");
-//        input.put("isPassenger", true);
-//
-//        assertThrows(Exception.class, () -> accountService.signup(input), "Invalid cpf");
-//    }
-//
-//    @Test
-//    public void shouldNotCreatePassengerWithInvalidName() {
-//        Map<String, Object> input = new HashMap<>();
-//        input.put("name", "John");
-//        input.put("email", "john.doe" + Math.random() + "@gmail.com");
-//        input.put("cpf", "95818705552");
-//        input.put("isPassenger", true);
-//
-//        assertThrows(Exception.class, () -> accountService.signup(input), "Invalid name");
-//    }
-//
-//    @Test
-//    public void shouldNotCreatePassengerWithInvalidEmail() {
-//        Map<String, Object> input = new HashMap<>();
-//        input.put("name", "John Doe");
-//        input.put("email", "john.doe" + Math.random() + "@");
-//        input.put("cpf", "95818705552");
-//        input.put("isPassenger", true);
-//
-//        assertThrows(Exception.class, () -> accountService.signup(input), "Invalid email");
-//    }
-//
-//    @Test
-//    public void shouldNotCreateDuplicatePassenger() throws Exception {
-//        Map<String, Object> input = new HashMap<>();
-//        input.put("name", "John Doe");
-//        input.put("email", "john.doe" + Math.random() + "@gmail.com");
-//        input.put("cpf", "95818705552");
-//        input.put("isPassenger", true);
-//
-//        accountService.signup(input);
-//        assertThrows(Exception.class, () -> accountService.signup(input), "Account already exists");
-//    }
-//
-//    @Test
-//    public void shouldCreateDriver() throws Exception {
-//        Map<String, Object> input = new HashMap<>();
-//        input.put("name", "John Doe");
-//        input.put("email", "john.doe" + Math.random() + "@gmail.com");
-//        input.put("cpf", "95818705552");
-//        input.put("carPlate", "AAA9999");
-//        input.put("isDriver", true);
-//
-//        Map<String, Object> output = accountService.signup(input);
-//        assertNotNull(output.get("accountId"));
-//    }
-//
-//    @Test
-//    public void shouldNotCreateDriverWithInvalidCarPlate() {
-//        Map<String, Object> input = new HashMap<>();
-//        input.put("name", "John Doe");
-//        input.put("email", "john.doe" + Math.random() + "@gmail.com");
-//        input.put("cpf", "95818705552");
-//        input.put("carPlate", "AAA999");
-//        input.put("isDriver", true);
-//
-//        assertThrows(Exception.class, () -> accountService.signup(input), "Invalid plate");
-//    }
+    private SignupService signupService;
+    private GetAccountByIdService getAccountByIdService;
+
+    @BeforeEach
+    public void setUp() {
+        AccountPersistenceOutboundPort accountPersistenceOutboundPort = new InMemoryAccountClientAdapter();
+        SendEmailOutboundPort sendEmailOutboundPort = mock(SendEmailOutboundPort.class);
+
+        signupService = new SignupService(new CpfValidator(), accountPersistenceOutboundPort, sendEmailOutboundPort);
+        getAccountByIdService = new GetAccountByIdService(accountPersistenceOutboundPort);
+    }
+
+    @Test
+    public void shouldCreatePassenger() {
+        SignupAccount input = SignupAccountTestsHelper.buildSimplePassenger();
+
+        String accountId = signupService.execute(input);
+        Optional<Account> accountOptional = getAccountByIdService.execute(accountId);
+
+        assertTrue(accountOptional.isPresent());
+        Account output = accountOptional.get();
+        assertEquals(input.name(), output.name());
+        assertEquals(input.email(), output.email());
+        assertEquals(input.cpf(), output.cpf());
+        assertEquals(input.carPlate(), output.carPlate());
+        assertEquals(input.isPassenger(), output.isPassenger());
+        assertEquals(input.isDriver(), output.isDriver());
+        assertEquals(input.isVerified(), output.isVerified());
+    }
+
+    @Test
+    public void shouldCreateDriver() {
+        SignupAccount input = SignupAccountTestsHelper.buildSimpleDriver();
+
+        String accountId = signupService.execute(input);
+        Optional<Account> accountOptional = getAccountByIdService.execute(accountId);
+
+        assertTrue(accountOptional.isPresent());
+        Account output = accountOptional.get();
+        assertEquals(input.name(), output.name());
+        assertEquals(input.email(), output.email());
+        assertEquals(input.cpf(), output.cpf());
+        assertEquals(input.carPlate(), output.carPlate());
+        assertEquals(input.isPassenger(), output.isPassenger());
+        assertEquals(input.isDriver(), output.isDriver());
+        assertEquals(input.isVerified(), output.isVerified());
+    }
+
+    @Test
+    public void shouldNotCreatePassengerWithInvalidName() {
+        SignupAccount input = SignupAccountTestsHelper.buildPassengerWithInvalidName();
+
+        assertThrows(Exception.class, () -> signupService.execute(input), "Invalid name");
+    }
+
+    @Test
+    public void shouldNotCreatePassengerWithInvalidCpf() {
+        SignupAccount input = SignupAccountTestsHelper.buildPassengerWithInvalidCpf();
+
+        assertThrows(Exception.class, () -> signupService.execute(input), "Invalid cpf");
+    }
+
+    @Test
+    public void shouldNotCreatePassengerWithInvalidEmail() {
+        SignupAccount input = SignupAccountTestsHelper.buildPassengerWithInvalidEmail();
+
+        assertThrows(Exception.class, () -> signupService.execute(input), "Invalid email");
+    }
+
+    @Test
+    public void shouldNotCreateDriverWithInvalidCarPlate() {
+        SignupAccount input = SignupAccountTestsHelper.buildDriverWithInvalidCarPlate();
+
+        assertThrows(Exception.class, () -> signupService.execute(input), "Invalid plate");
+    }
+
+    @Test
+    public void shouldNotCreateDuplicatePassenger() {
+        SignupAccount input = SignupAccountTestsHelper.buildSimplePassenger();
+
+        signupService.execute(input);
+        assertThrows(Exception.class, () -> signupService.execute(input), "Account already exists");
+    }
+
 }
